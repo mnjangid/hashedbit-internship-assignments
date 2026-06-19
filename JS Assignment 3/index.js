e2f4bba4b57ddf4862ac2ce585f9632f2b52606a
mnjangid <mnjangid9428@gmail.com>
2026-06-19 16:53:35 +0530

    Solved first problem

---
+++ JS Assignment 3/index.js
@@ -0,0 +1,36 @@
+const array = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal"
+];
+
+const newArray = array.filter(function(value){
+    if(value.startsWith('A') || value.startsWith('E') || (value.startsWith('I') || value.startsWith('O') || value.startsWith('U'))) return false;
+    return true;
+});
+console.log(newArray);
